c5bbd3c98a733df2f2853829a0c2adbb0a6374a5
Edson L B Filho <edcastanha@gmail.com>
2024-01-08 07:55:22 -0300

    fix: correcoes de bug de sincronizacao do computador, fazer build para teste

--- Poc_Api_Rest_Docker/django-server/core/helpers/tasks.py
+++ Poc_Api_Rest_Docker/django-server/core/helpers/tasks.py
@@ -10,9 +10,8 @@ def run_webScrappingTask(task_instance):
         inicio = timezone.now()
         logger.debug(f'Tarefa de scrapping iniciada: {inicio}')
         
-        api_url = "http://localhost:5000/scrapping"
         data_info = task_instance.id_informacao_alvo.get_data
-        response = requests.post(api_url, data={'action': 'created', 'body': task_instance})
+        
         logger.debug(f'Resposta da API: {response}')
     except requests.exceptions.RequestException as e:
         logger.error(f'Erro de solicitação: {e}')
--- Poc_Api_Rest_Docker/django-server/core/webScrappingTask/models.py
+++ Poc_Api_Rest_Docker/django-server/core/webScrappingTask/models.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from core.helpers.tasks import run_webScrappingTask
 from core.helpers.loggingMe import logger
 
+
 # Classe base para todos os modelos, contendo informações de data de cadastro e atualização
 class BaseModel(models.Model):
     data_cadastro = models.DateTimeField(auto_now_add=True)
@@ -12,35 +13,41 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-# Modelo para salvar informações sobre as URLs de destino, códigos de acesso, status e caminhos de arquivos
+
 class InformacaoAlvo(BaseModel):
     # Opções para o campo 'status'
     CHOICE_STATUS = (
-        ('Aguardando', 'Aguardando'), 
-        ('Finalizado', 'Finalizado'),
-        ('Error', 'Error'),
-    ) 
+        ("Aguardando", "Aguardando"),
+        ("Processando", "Processando"),
+        ("Finalizado", "Finalizado"),
+        ("Error", "Error"),
+    )
     token = models.CharField(max_length=100, unique=True)
     url_alvo = models.URLField()
     codigo_acesso = models.CharField(max_length=12, blank=False)
-    status = models.CharField(max_length=10, choices=CHOICE_STATUS, default='Aguardando')
-    url_arquivo = models.URLField()  # URL de arquivo: bucket S3, link de download ou FTP
+    status = models.CharField(
+        max_length=10, choices=CHOICE_STATUS, default="Aguardando"
+    )
+    url_arquivo = (
+        models.URLField()
+    )  # URL de arquivo: bucket S3, link de download ou FTP
 
     def __str__(self):
         return f"Código: {self.codigo_acesso}, URL: {self.url_alvo}"
-    
+
     def get_data(self):
         # Retorna dados formatados para a instância InformacaoAlvo
         return {
-            'id': self.id,
-            'token': self.token,
-            'url_alvo': self.url_alvo,
-            'codigo_acesso': self.codigo_acesso,
-            'status': self.status,
-            'path_arquivo': self.url_arquivo,  # Corrigido para 'url_arquivo'
-            'data_atualizado': self.data_atualizacao.strftime("%d/%m/%Y %H:%M:%S"),
+            "id": self.id,
+            "token": self.token,
+            "url_alvo": self.url_alvo,
+            "codigo_acesso": self.codigo_acesso,
+            "status": self.status,
+            "path_arquivo": self.url_arquivo,  # Corrigido para 'url_arquivo'
+            "data_atualizado": self.data_atualizacao.strftime("%d/%m/%Y %H:%M:%S"),
         }
-    
+
+
 class Tarefas(BaseModel):
     id_informacao_alvo = models.ForeignKey(InformacaoAlvo, on_delete=models.CASCADE)
     tarefa = models.CharField(max_length=100)
@@ -60,9 +67,8 @@ def tarefa_pre_save(sender, instance, created, **kwargs):
     if created:
         # Cria um dicionário com os dados da instância Tarefas e InformacaoAlvo
         task_data = {
-            'id_informacao_alvo': instance,
-            'tarefa': instance.token,
-            'data_inicio': instance.data_cadastro.strftime('%Y-%m-%d %H:%M:%S'),
+            "tarefa": instance.id,
+            "data_inicio": instance.data_cadastro.strftime("%Y-%m-%d %H:%M:%S"),
         }
         # Dispara a tarefa do Celery passando os dados das instâncias Tarefas e InformacaoAlvo como argumento
         run_webScrappingTask.delay(task_data)
