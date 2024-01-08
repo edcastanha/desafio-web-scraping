c5200ca64374c08460173e7af883cc4a481fe46b
Edson L B Filho <edcastanha@gmail.com>
2024-01-08 14:22:14 -0300

    refactor: simplificando a aplicacao via API > QUEUE > SCRAPPING

--- Poc_Api_Rest_Docker/django-server/core/webScrappingTask/urls.py
+++ Poc_Api_Rest_Docker/django-server/core/webScrappingTask/urls.py
@@ -1,6 +1,7 @@
-from django.urls import include, path
+from django.urls import path, include  # Adicionando 'include'
 from rest_framework import routers
 from core.webScrappingTask.api import TarefasViewSet, InformacaoAlvoViewSet
+from core.webScrappingTask.views import index, new_target  # Importando as views corretamente
 
 # URLS API
 router = routers.DefaultRouter()
@@ -8,11 +9,9 @@ router.register(r'tasks', TarefasViewSet, basename='tasks')
 router.register(r'targets', InformacaoAlvoViewSet, basename='targets')
 
 # WEB APP
-from core.webScrappingTask import views
-
 urlpatterns = [
-    # WEB APP
-    path('', views.index, name="index"),
-
+    path('', index, name='index'),
+    path('api/new_goal/', new_target, name='new_target'),  # Corrigindo o caminho da nova meta
+    path('api/', include(router.urls)),  # Adicionando as rotas da API
+    # outras URLs...
 ]
-
