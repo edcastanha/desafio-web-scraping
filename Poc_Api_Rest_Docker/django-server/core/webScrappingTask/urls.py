682244aa0912ce8497e87160cacfa654e5c27731
Edson L B Filho <edcastanha@gmail.com>
2024-01-08 17:27:11 -0300

    fix: RESETA DJANGO

--- Poc_Api_Rest_Docker/django-server/core/webScrappingTask/urls.py
+++ Poc_Api_Rest_Docker/django-server/core/webScrappingTask/urls.py
@@ -1,17 +1,8 @@
-from django.urls import path, include
-from rest_framework import routers
-from core.webScrappingTask.api import TarefasViewSet, InformacaoAlvoViewSet
-from core.webScrappingTask.views import index, new_target
-
-# Configuração das rotas da API usando DRF Router
-router = routers.DefaultRouter()
-router.register(r'tasks', TarefasViewSet, basename='tasks')
-router.register(r'targets', InformacaoAlvoViewSet, basename='targets')
+from django.urls import path
+from core.webScrappingTask.views import index, new_goal
 
 # Configuração das URLs da aplicação web
 urlpatterns = [
-    path('', index, name='index'),  # Rota para a view 'index' da aplicação web
-    path('new_goal/', new_target, name='new_target'),  # Rota para a view 'new_target' da aplicação web
-    path('api/', include(router.urls)),  # Incluindo as rotas da API usando o 'include'
-    # outras URLs...
+    path('', index, name='index'),
+    path('new_goal/', new_goal, name='new_goal'),
 ]
--- Poc_Api_Rest_Docker/django-server/core/webScrappingTask/views.py
+++ Poc_Api_Rest_Docker/django-server/core/webScrappingTask/views.py
@@ -1,65 +1,8 @@
-from rest_framework.decorators import api_view
 from django.middleware.csrf import get_token
 from django.shortcuts import render
-from rest_framework import status
-from django.http import JsonResponse
-from rest_framework.response import Response
-
-#from django.views.decorators.csrf import csrf_exempt
-
-from core.webScrappingTask.serializers import InformacaoAlvoSerializer
-from core.webScrappingTask.models import InformacaoAlvo
-
-import json
-from django.core.exceptions import ObjectDoesNotExist
 
 # === Frontend ===
 def index(request):
     key = {}
-    key['csrf_token'] = get_token(request)
+    key["csrf_token"] = get_token(request)
     return render(request, "index.html", key)
-
-@api_view(['GET', 'POST'])
-def informacao_alvo_view(request):
-    if request.method == 'GET':
-        try:
-            informacoes = InformacaoAlvo.objects.all()
-            serializer = InformacaoAlvoSerializer(informacoes, many=True)
-            return Response(serializer.data)
-        except ObjectDoesNotExist as e:
-            return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    elif request.method == 'POST':
-        payload = json.loads(request.body)
-        try:
-            target = InformacaoAlvo.objects.create(
-                token=payload["token"],
-                url_alvo=payload["url"],
-                codigo_acesso=payload["codigo"],
-            )
-            serializer = InformacaoAlvoSerializer(target)
-            return JsonResponse({'target': serializer.data}, safe=False, status=status.HTTP_201_CREATED)
-        except ObjectDoesNotExist as e:
-            return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-# ==== API ====
-@api_view(['POST'])
-def new_target(request):
-    payload = json.loads(request.body)
-    try:
-        target = InformacaoAlvo.objects.create(
-            token=payload["token"],
-            url_alvo=payload["url"],
-            codigo_acesso=payload["codigo"],
-        )
-        serializer = BookSerializer(book)
-        return JsonResponse({'target': serializer.data}, safe=False, status=status.HTTP_201_CREATED)
-    except ObjectDoesNotExist as e:
-        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
-    except Exception:
-        return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-     
