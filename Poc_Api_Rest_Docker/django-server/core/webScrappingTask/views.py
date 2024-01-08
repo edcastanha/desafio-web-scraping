49e4a4359ab357186d80321e1e15370abc06c638
Edson L B Filho <edcastanha@gmail.com>
2024-01-08 18:46:35 -0300

    feat: Cria Forms e valida comunicacao das filas

--- Poc_Api_Rest_Docker/django-server/core/webScrappingTask/views.py
+++ Poc_Api_Rest_Docker/django-server/core/webScrappingTask/views.py
@@ -1,18 +1,25 @@
-from django.middleware.csrf import get_token
 from django.shortcuts import render
-from core.webScrappingTask.forms import TargetInformationForm
 from core.helpers.loggingMe import logger
+from core.webScrappingTask.forms import TargetInformationForm
+from core.webScrappingTask.models import InformacaoAlvo
 
-# === Frontend ===
+
 def index(request):
     if request.method == 'POST':
         form = TargetInformationForm(request.POST)
         if form.is_valid():
-            novo_alvo = form.save()  # Salvar o objeto InformacaoAlvo
-            logger.info(f"TargetInformationForm:: {novo_alvo}")
+            novo_alvo = form.save(commit=False)  # Não salve no banco de dados ainda
+            token_csrf = request.POST.get('csrfmiddlewaretoken')  # Obtém o csrf_token do POST
+            novo_alvo.token = token_csrf  # Atribui o csrf_token ao campo token
+            
+            novo_alvo.save()  # Salva o objeto InformacaoAlvo com o csrf_token
+            
+            id_salvo = novo_alvo.id
+            token_salvo = novo_alvo.token
+            
+            # Agora você pode retornar esses dados para o template
+            return render(request, 'index.html', {'form': form, 'token_salvo': token_salvo, 'id_salvo': id_salvo})
     else:
         form = TargetInformationForm()
     
     return render(request, 'index.html', {'form': form})
-
-
